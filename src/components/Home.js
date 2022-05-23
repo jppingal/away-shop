22a07399a618efa4500fbc6089b83db0e41de114
Jai Prakash <jp.pingal1993@gmail.com>
2022-05-23 09:13:41 +0530

    fatch data to api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,49 @@
-import React,{useEffect, useState} from "react"
-import {useSelector} from "react-redux";
+import React, { useEffect, useState } from "react"
+import axios from 'axios';
+import { useSelector } from "react-redux";
 import { useDispatch } from 'react-redux';
 import Header from "./Header";
 import ProductDetails from "./ProductDetails";
 import Products from "./Products";
 import { Link } from "react-router-dom";
-import{setProducts} from "../redux/action/productAction"
+import { setProducts } from "../redux/action/productAction"
 
 const Home = () => {
-    const dispatch = useDispatch()
-    const products = useSelector((state)=>state.allProducts.products)
-    const [myProducts, setMyProducts] = useState()
-    console.log("myProducts :",myProducts)
+  const dispatch = useDispatch()
+  const products = useSelector((state) => state.allProducts.products)
+  const [myProducts, setMyProducts] = useState()
+  // useEffect(() => {
+	// 	const fetchApi = async () => {
+	// 		const url = "https://fakestoreapi.com/products";
+	// 		const res = await fetch(url);
+	// 		const resJson = await res.json();
+  //     console.log(resJson)
+  //     dispatch(setProducts(resJson))
+	// 		setMyProducts(resJson);
+	// 	}
+	// 	fetchApi();
+	// }, [])
+  const fetchApi = async () => {
+    const response = await axios.get("https://fakestoreapi.com/products")
+    .then((fatchData)=> {return fatchData.json()
+      dispatch(setProducts(fatchData))
+    })
+    .catch((err) => {
+      console.log("err", err)
+    })
 
-    useEffect(() => {
-      const fatchProducts = async () => {
-        const url = "https://fakestoreapi.com/products";
-        const res = await fetch(url);
-        const resJson = await res.json();
-        dispatch(setProducts(resJson));
-      }
-      fatchProducts();
-    }, [])
-    // useEffect(()=>{
-    // const fatchProducts = async() =>{
-    //   const getData = await fetch('https://fakestoreapi.com/products')
-     
-    //   .then((response) => { 
-    //     return response.json()
-    //   })
-    //   .then((data) => { 
-    //     return  data
-    //     dispatch(setProducts(data))
-    //   }).catch((error)=>{
-    //     return console.log("error :", error)
-    //   })
-      
-    // }
-    
-    //   fatchProducts()
-    // },[])
+  }
 
-    console.log("products", products)
-    return(
-        <>
-        <div className="main-container">
-          <Products/>
-        </div>
-        </>
-    )
+  useEffect(() => {
+    fetchApi();
+  }, [])
+  console.log("products", products)
+  return (
+    <>
+      <div className="main-container">
+        <Products />
+      </div>
+    </>
+  )
 }
 export default Home;
